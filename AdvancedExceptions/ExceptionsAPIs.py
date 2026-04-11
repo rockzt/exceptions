66f37b0237bb2646f6149740b889f28ad0ad151b
Rodrigo Zavala <rodrigoztrejo@gmail.com>
2026-04-11 12:48:38 -0600

    [added] implementing context manager on db connection

--- AdvancedExceptions/ExceptionsAPIs.py
+++ AdvancedExceptions/ExceptionsAPIs.py
@@ -1,7 +1,19 @@
 import sqlite3
 
-from IPython.core.pylabtools import retina_figure
-
+# Creating context manager to db connection
+# (creation and destruction of the connection)
+class SQLite:
+    def __init__(self, file='application.db'):
+        self.file = file
+    def __enter__(self):
+        # Connecting to the database
+        self.conn = sqlite3.connect(self.file)
+        # Returning a cursor to execute queries
+        return self.conn.cursor()
+    def __exit__(self, type, value, traceback):
+        # Closing connection when the block is exited,
+        # even if an exception is raised
+        self.conn.close()
 
 class NotFoundError(Exception):
     pass
@@ -17,26 +29,20 @@ def blog_lst_to_json(item):
             'public': item[3],
         }
 
+# Implementing context manager to handle database connection and exceptions
 def fetch_blogs():
     try:
-        # connect to the database
-        con = sqlite3.connect('blogs.db')
-        cur = con.cursor()
+        with SQLite('blogs.db') as cur:
+            # execute query
+            cur.execute("SELECT * FROM blogs")
+            # fetch the data and turn it into a dict
+            result = list(map(blog_lst_to_json() , cur.fetchall()))
 
-        # execute query
-        cur.execute("SELECT * FROM blogs")
-        # fetch the data and turn it into a dict
-        result = list(map(blog_lst_to_json() , cur.fetchall()))
-
-        return result
+            return result
     # Not recommended
     except Exception as e:
         print(e)
         return []
-
-    finally:
-        # Close the database connection
-        con.close()
 
 
 def fetch_blog(id: str):
